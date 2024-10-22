ae1f32a5030b9322308bc6e6d5fe820c3add5046
farnejos000 <farnejos000@alpinesd.org>
2024-10-22 11:14:27 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,6 +1,12 @@
 #while summation code following directions from Readme
 n=int(input("enter a number:"))
 t=0
-for t in range(n):
-   t=t+1
-   print(t)
+p=0
+c=0
+while n>t:
+    t=t+1
+    print(t)
+while n>p:
+    p=p+1
+    c=p+c
+print("the answer is",c)
